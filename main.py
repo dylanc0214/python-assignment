0275fd602bddb1fb26341c0caad7310b5ed7d794
dylanc0214 <dylanchow0214@gmail.com>
2024-12-20 13:33:39 +0800

    main

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # Main menu
 # Users need to choose the option (Admin, Student, Teacher, Staff)
 def main_menu():
+    # Can always ask the users
     while True:
 
         # Show the Options
-        print("--------------")
+        print("-----Welcome-----")
         print("1. Student")
         print("2. Teacher")
         print("3. Staff")
@@ -32,11 +33,18 @@ def main_menu():
         # If users type other than 1, 2, 3, 4, 5 then will proceed this
         else:
             print("Invalid choice")
-
+# Student Menu
 def student_menu():
-
+    print("-----Student Menu-----")
+# Teacher Menu
 def teacher_menu():
-
+    print("-----Teacher Menu-----")
+# Staff Menu
 def staff_menu():
-
+    print("-----Staff Menu-----")
+# Admin Menu
 def admin_menu():
+    print("-----Admin Menu-----")
+
+# Start the program
+main_menu()
